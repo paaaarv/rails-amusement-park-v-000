af0d102b8add867333cfd7298c412d0e022d7460
Parv Kumar <parvathikumar92@gmail.com>
2018-02-22 22:30:04 +0000

    finished DB info and model tests

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,3 +1,12 @@
 class Attraction < ActiveRecord::Base
   # write associations here
+  validates :name, presence: true
+  validates :tickets, presence: true
+  validates :nausea_rating, presence: true
+  validates :min_height, presence: true
+  validates :happiness_rating, presence: true
+
+  has_many :rides
+  has_many :users, through: :rides
+
 end
