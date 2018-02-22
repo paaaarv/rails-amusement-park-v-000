e6978d2327b0e01cab26bf82df689fdd7acf6af9
Parv Kumar <parvathikumar92@gmail.com>
2018-02-22 22:14:38 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,13 @@ class User < ActiveRecord::Base
   # write associations here
   has_secure_password
   has_many :rides
-  has_many :attractions, through: :rides 
+  has_many :attractions, through: :rides
 
-  def mood 
-    if self.nausea > self.happiness 
+  def mood
+    if self.nausea > self.happiness
       "sad"
-    else 
-      "happy" 
+    else
+      "happy"
     end 
-  end 
+  end
 end
