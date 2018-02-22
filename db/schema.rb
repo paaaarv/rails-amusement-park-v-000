ca788b2609fed7cee652c214befc72ebda079ef6
Parv Kumar <parvathikumar92@gmail.com>
2018-02-22 22:17:04 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140703140419) do
+ActiveRecord::Schema.define(version: 20180222221531) do
 
   create_table "attractions", force: :cascade do |t|
     t.string  "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20140703140419) do
     t.integer "nausea"
     t.integer "tickets"
     t.integer "height"
+    t.boolean "admin",           default: false
   end
 
 end
