063be1714f18400dafcfc187f07d33b4aea03391
Parv Kumar <parvathikumar92@gmail.com>
2018-02-22 22:04:31 +0000

    automatically backed up by learn

--- db/migrate/20140703140419_create_rides.rb
+++ db/migrate/20140703140419_create_rides.rb
@@ -4,7 +4,7 @@ class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
       t.integer :user_id
-      t.integer :attraction_id 
-    end 
+      t.integer :attraction_id
+    end
   end 
 end
