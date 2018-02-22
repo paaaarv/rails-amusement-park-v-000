89fff2d097a0ec6c88a131fbd1063f6c183726bf
Parv Kumar <parvathikumar92@gmail.com>
2018-02-22 22:05:02 +0000

    automatically backed up by learn

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,3 +1,9 @@
 class Attraction < ActiveRecord::Base
   # write associations here
+  validates :name, presence: true
+  validates :tickets, presence: true
+  validates :nausea_rating, presence: true
+  validates :min_height, presence: true
+  validates :happiness_rating, presence: true 
+
 end
