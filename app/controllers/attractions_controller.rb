4b93dabfba68d411683b027df8db649902354586
Parv Kumar <parvathikumar92@gmail.com>
2018-03-01 19:51:35 +0000

    added create route to save new attraction

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -12,6 +12,22 @@ class AttractionsController < ApplicationController
    end
 
    def new
+     @attraction = Attraction.new
    end
 
+   def create
+     @attraction = Attraction.new(attraction_params)
+
+     if @attraction.save
+       redirect_to attraction_path(@attraction)
+     else
+       redirect_to new_attraction_path
+     end
+   end
+
+   private
+
+   def attraction_params
+     params.require(:attraction).permit(:name, :min_height, :happiness_rating, :nausea_rating, :tickets)
+   end
 end
